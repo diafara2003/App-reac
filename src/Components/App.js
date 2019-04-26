555b61afa5ff36e03e5bcd9eb3a559ab9b4db55d
jhonnatan uruena <jaud_@hotmail.com>
2019-04-25 21:34:48 -0500

    :estructura inicial

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">        
+        <p>
+        reacjs
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
