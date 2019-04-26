d1fc677169b8b15633675423b6e5ad25e17b3bb3
Jhonnatan Urueña <jhonnatan.uruea@SINCO.NET>
2019-04-26 10:46:21 -0500

    :formulario

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import '../css/App.css'
+import React, { Component } from "react";
+import "../css/App.css";
 
 //comportents
-import Header from './Header';
-import FormGastos from './FrmGastos';
-import Listado from './Listado';
-import ControlPpto from './ControlPpto';
-
+import Header from "./Header";
+import FormGastos from "./FrmGastos";
+import Listado from "./Listado";
+import ControlPpto from "./ControlPpto";
+import NumberFormat from 'react-number-format';
 
 //helpers
-import { validarPpto } from '../helper';
-
-
+import { validarPpto } from "../helper";
 
 class App extends Component {
   VrPpto = React.createRef();
@@ -20,27 +18,32 @@ class App extends Component {
     gastos: [],
     presupuesto: 0,
     restante: 0
-  }
+  };
 
   // componentDidMount() {
   //   this.obtenerPpto();
   // }
 
   obtenerPpto = () => {
+    let _vrPpto = this.VrPpto.current.value;
 
-    const _vrPpto = this.VrPpto.current.value;
+    if (_vrPpto == "") {
+      _vrPpto = 0;
+    }
 
-    if (validarPpto(_vrPpto)) {
-
+    if (!validarPpto(_vrPpto)) {
+      this.setState({
+        gastos: [],
+        presupuesto: 0,
+        restante: 0
+      });
+    } else {
       this.setState({
         presupuesto: _vrPpto,
         restante: _vrPpto
       });
     }
-
-  }
-
-
+  };
 
   agregarGasto = gasto => {
     const _gastos = { ...this.state.gastos };
@@ -53,61 +56,53 @@ class App extends Component {
 
     this.restarPpto(gasto.cantidad);
     // console.log(_gastos);
-  }
+  };
 
-  restarPpto = (vrGasto) => {
+  restarPpto = vrGasto => {
     const resultado = this.state.restante - vrGasto;
     this.setState({
       restante: resultado
     });
-  }
-
+  };
 
   render() {
+    const { presupuesto } = this.state;
     return (
-      <div className="App container" >
-        <Header
-          title="Gastos quincenales">
-        </Header>
+      <div className="App container">
+        <Header title="Gastos quincenales" />
 
         <div className="contenido-principal contenido">
           <div className="row">
             <div className="campo">
               <label>Ingrese presupuesto</label>
-              <input onBlur={this.obtenerPpto} ref={this.VrPpto} type="text" className="u-full-width" placeholder="Ej. 1,000,000"></input>
-
-
-            </div>
+              <input
+                onBlur={this.obtenerPpto}
+                ref={this.VrPpto}
+                type="text"
+                className="u-full-width"
+                placeholder="Ej. 1,000,000"
+              />
+            </div>            
           </div>
+          { presupuesto == 0 ? null : (
+            <div className="row">
+              <div className="one-half column">
+                <FormGastos agregarGasto={this.agregarGasto} />
+              </div>
+              <div className="one-half column">
+                <Listado gastos={this.state.gastos} />
 
-
-          <div className="row">
-
-            <div className="one-half column">
-              <FormGastos
-                agregarGasto={this.agregarGasto}
-              >
-
-              </FormGastos>
+                <ControlPpto
+                  presupuesto={this.state.presupuesto}
+                  restante={this.state.restante}
+                />
+              </div>
             </div>
-            <div className="one-half column">
-              <Listado
-                gastos={this.state.gastos}
-              ></Listado>
-
-              <ControlPpto
-                presupuesto={this.state.presupuesto}
-                restante={this.state.restante}
-              ></ControlPpto>
-
-            </div>
-          </div>
+          )}
         </div>
       </div>
     );
   }
-
-
 }
 
 export default App;
--- src/Components/FrmGastos.js
+++ src/Components/FrmGastos.js
@@ -1,47 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class FrmGastos extends Component {
+  //reaf para los campor el formulario
+  nombreGasto = React.createRef();
+  cantidad = React.createRef();
 
-    //reaf para los campor el formulario
-    nombreGasto = React.createRef();
-    cantidad = React.createRef();
+  crearGasto = e => {
+    e.preventDefault();
 
+    const gasto = {
+      nombre: this.nombreGasto.current.value,
+      cantidad: this.cantidad.current.value
+    };
 
-    crearGasto = (e) => {
-        e.preventDefault();
-
-        const gasto = {
-            nombre: this.nombreGasto.current.value,
-            cantidad: this.cantidad.current.value
-        };
-      
-
-        this.props.agregarGasto(gasto);
-
-        e.currentTarget.reset();
+    if (gasto.nombre == "" || gasto.cantidad == "") {
+      return null;
     }
 
+    this.props.agregarGasto(gasto);
 
-    render() {
+    e.currentTarget.reset();
+  };
 
-        return (
-            <form onSubmit={this.crearGasto}>
-                <h2>Agrega tus gastos aqui</h2>
-                <div className="campo">
-                    <label>Nombre gasto</label>
-                    <input ref={this.nombreGasto} type="text" className="u-full-width" placeholder="Ej. transporte"></input>
-                </div>
-                <div className="campo">
-                    <label>Cantidad</label>
-                    <input ref={this.cantidad} type="number" className="u-full-width" placeholder="Ej. 300"></input>
-                </div>
+  render() {
+    return (
+      <form onSubmit={this.crearGasto}>
+        <h2>Agrega tus gastos aqui</h2>
+        <div className="campo">
+          <label>Nombre gasto</label>
+          <input
+            ref={this.nombreGasto}
+            type="text"
+            className="u-full-width"
+            placeholder="Ej. transporte"
+          />
+        </div>
+        <div className="campo">
+          <label>Cantidad</label>
+          <input
+            ref={this.cantidad}
+            type="number"
+            className="u-full-width"
+            placeholder="Ej. 300"
+          />
+        </div>
 
-                <input type="submit" className="button-primary u-full-width" value="Agregar"></input>
-
-            </form>
-        )
-    }
-
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Agregar"
+        />
+      </form>
+    );
+  }
 }
 
 export default FrmGastos;
